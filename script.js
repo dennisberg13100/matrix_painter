b60a60597e773d5109f40831a0cb38b8070b2eb4
Dennis <dennisvandenberg13100@gmail.com>
2020-08-12 15:14:36 +0200

    do and Undo through hotkeys ctrl z and ctrl schift z added

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ const settings = {
 	values: ["0"],
 	margin: 0,
 	selected: 0
-}
+};
 
-// test function 
-function test(e){
-	alert("test")
+const paintHistory = {
+	stepList: [],
+	undoIndex: -1,
 }
 
 // Create a new empty matrix filled with zeros
@@ -32,6 +32,7 @@ function createNewMatrix(obj) {
 			matrix[i].push(0)
 		}
 	}
+
 	return matrix;
 }
 
@@ -70,23 +71,21 @@ function createCanvas(div, canvasId, obj){
 
 // Define a color as selected 
 function selectColor(e) {
+
 	let elements = document.getElementsByClassName('selected');
+
 	while(elements.length > 0){
 	    elements[0].classList.remove('selected');
 	}
 	let index =  e.getAttribute("data");
-	console.log(index)
 	settings.selected = index;
 	e.classList.add("selected");
-	console.log(e);
 }
 
 // Draw the color menu
 function drawColorMenu(colors, values, selected, div) {
 	let content = "";
-	let clr = "colors";
-	let val = "values";
-	console.log(content);
+	
 	for( let i = 0; i < colors.length; i++){
 		content += '<div class="color-selector ';
 		if ( i == selected ){
@@ -99,26 +98,32 @@ function drawColorMenu(colors, values, selected, div) {
 		content += "'values'" + ')" data="' + i + '"></div>';
 		content += '</div>';
 	}
-	console.log(content);
+
 	div.innerHTML = content;
 }
 
 // Write the matrix preview in the matrixPreviw area 
 function writeMatrixInDiv(matrix, values, div) {
+
 	let content = "[";
+
 	for(let i = 0; i < matrix.length; i++){
+
 		if( i != 0 ){
 			content += "&nbsp;[";
 		} else {
 			content += "[";
 		}
+
 		for ( let j = 0; j < matrix[i].length; j++) {
 			content += values[matrix[i][j]];
 			if ( j != (matrix[i].length - 1)){
 				content += ", ";
 			}
 		}
+
 		content += "]";
+
 		if( i != (matrix.length-1)){
 			content += ",</br>";
 		}
@@ -131,13 +136,28 @@ function writeMatrixInDiv(matrix, values, div) {
 // Make the actions when you click on the canvas
 function clickOnCanvas(e){
 	let canvas = document.getElementById(canvasId).getBoundingClientRect();
-	x = e.clientX - canvas.x;
-	y = e.clientY - canvas.y;
+	let x = e.clientX - canvas.x;
+	let y = e.clientY - canvas.y;
+
 	x = Math.floor(x / settings.squareSize);
 	y = Math.floor(y / settings.squareSize);
+	position = [x, y];
+	addChangeToHistory(position);
+	paintHistory.stepList = paintHistory.stepList.slice(0,(paintHistory.undoIndex +1))
 	settings.matrix[x][y] = parseInt(settings.selected);
 	renderWorkingArea(canvasDiv, canvasId, settings);
-	console.log(settings.matrix);
+}
+
+function addChangeToHistory (position) {
+	let valueIndex = settings.matrix[position[0]][position[1]];
+	const data = {
+		position: position,
+		oldValue: valueIndex,
+		newValue: settings.selected
+	}
+	paintHistory.undoIndex +=1;
+	paintHistory.stepList.push(data);
+
 }
 
 // This function takes a Matrix a old value and a new value and it returns the new matrix with the new values in the place of teh old ones
@@ -157,7 +177,6 @@ function updateMatrixItem(e, colorValue){
 	let value = e.value;
 	let index =  e.getAttribute("data");
 	let oldValue = settings.values[index];
-	console.log(oldValue);
 	if (value != "") {
 		if (colorValue == "colors"){
 			settings.colors[index] = value;
@@ -165,7 +184,6 @@ function updateMatrixItem(e, colorValue){
 			// settings.matrix = updateMatrixValues(settings.matrix, oldValue, value);
 			settings.values[index] = value;
 		}
-		console.log(settings)
 		renderWorkingArea(canvasDiv, canvasId, settings);
 	}	
 }
@@ -174,6 +192,7 @@ function updateMatrixItem(e, colorValue){
 function addNewColor() {
 	settings.colors.push("#FFFFFF");
 	settings.values.push("0");
+
 	drawColorMenu(settings.colors, settings.values, settings.selected, colorMenu);
 }
 
@@ -190,7 +209,7 @@ function drawSquare(x, y, color, squareSize, ctx) {
 	ctx.fillStyle = color;
 	ctx.fillRect(x*squareSize , y*squareSize , squareSize , squareSize );
 
-	ctx.strokeStyle = "BLACK";
+	ctx.strokeStyle = "#333";
 	ctx.strokeRect(x*squareSize , y*squareSize , squareSize , squareSize );
 }
 
@@ -198,6 +217,7 @@ function drawSquare(x, y, color, squareSize, ctx) {
 function renderCanvas(canvasId, obj) {
 	const cvs = document.getElementById(canvasId); 
 	const ctx = cvs.getContext("2d");
+
 	for(let i = 0; i < obj.horizontalSquares ; i++){
 		for(let j = 0; j < obj.verticalSquares; j++){
 			index = obj.matrix[i][j];
@@ -231,20 +251,63 @@ window.addEventListener("resize", function(){renderWorkingArea(canvasDiv, canvas
 // Zoom in and out 
 function zoom(e){
 	if(e.deltaY > 0 && settings.squareSize > 10){
-		console.log("ZOOM OUT");
 		canvasDiv.innerHTML = "";
 		marginDelta = 5
+
 		for( let i = 0; i < marginDelta; i++){
 			settings.margin += 1;
 			canvasDiv.style.margin = settings.margin;
 			setTimeout(function(){ renderWorkingArea(canvasDiv, canvasId, settings); }, 50);
 		}
 	} else if(e.deltaY < 0 && settings.margin > 0)  {
-		console.log("ZOOM IN")
 		canvasDiv.innerHTML = "";
 		settings.margin -= 5;
 		canvasDiv.style.margin = settings.margin;
+
 		renderWorkingArea(canvasDiv, canvasId, settings);
 	}
 }
+
 window.addEventListener('wheel', function(e) {zoom(e);}, false);
+ 
+
+// ctrl+z function 
+document.addEventListener('keydown', (e) => {keyPress(e);});
+
+function keyPress(e){
+	if(e.keyCode === 90 && e.ctrlKey === true && e.shiftKey === false){
+		e.preventDefault();
+		undoStep(e);
+	} else if(e.keyCode === 90 && e.ctrlKey === true && e.shiftKey === true){
+		e.preventDefault();
+		redoStep(e);
+	}
+	
+}
+
+function undoStep(e){
+	e.preventDefault();
+	if( paintHistory.undoIndex > 0 ){
+		let index = paintHistory.undoIndex;
+		let data = paintHistory.stepList[index];
+		settings.matrix[data.position[0]][data.position[1]] = data.oldValue;
+		paintHistory.undoIndex -= 1;
+		renderCanvas(canvasId, settings);
+	} else if (paintHistory.undoIndex === 0) {
+		settings.matrix = createNewMatrix(settings);
+		paintHistory.undoIndex == -1;
+		renderCanvas(canvasId, settings);
+	}
+}
+
+// Now working 100% 
+function redoStep(e) {
+	e.preventDefault();
+	if ((paintHistory.stepList.length - 1) > paintHistory.undoIndex) {
+		paintHistory.undoIndex += 1;
+		let index = paintHistory.undoIndex;
+		let data = paintHistory.stepList[index];
+		settings.matrix[data.position[0]][data.position[1]] = data.newValue;
+		renderCanvas(canvasId, settings);
+	}
+}
