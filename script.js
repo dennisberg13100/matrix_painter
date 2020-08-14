adef18421f5ddbb74593c56e3edc78c13b3a64fe
Dennis <dennisvandenberg13100@gmail.com>
2020-08-14 16:50:53 +0200

    I put all the function in the order that they are being called. I  devide some big functions in smaller functions and put the x and y arguments in a object called position to reduce de amount of arguments

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const initialForm = document.getElementById('initialForm');
+const welcomePageForm = document.getElementById('welcomePageForm');
 const initialSettings =  document.getElementById('initialSettings');
 const workingArea = document.getElementById('workingArea');
 const canvasDiv = document.getElementById('canvasDiv');
@@ -22,6 +22,34 @@ const paintHistory = {
 	undoIndex: -1,
 }
 
+welcomePageForm.onsubmit = function (e){
+	e.preventDefault();
+	setSquareNumbers();
+	if ( settings.horizontalSquares > 0 && settings.verticalSquares > 0){
+		hiddeInitialPage();
+		showWorkingArea();
+		createNewMatrixFilledWithZeros();
+		renderWorkingArea();
+		drawColorMenu();
+
+	} else {
+		alert("Please fill in the amount of horizontal and vertical squares!");
+	}	
+};
+
+function setSquareNumbers(){
+	settings.horizontalSquares = document.getElementById('horizontal').value;
+	settings.verticalSquares = document.getElementById('vertical').value;
+}
+
+function hiddeInitialPage() {
+	initialSettings.style.display = 'none';
+}
+
+function showWorkingArea() {
+	workingArea.style.display = 'flex';
+}
+
 function createNewMatrixFilledWithZeros() {
 	let matrix = [];
 
@@ -32,49 +60,46 @@ function createNewMatrixFilledWithZeros() {
 		}
 	}
 
-	return matrix;
+	settings.matrix = matrix;
+}
+
+function renderWorkingArea() {
+	createCanvas();
+	renderCanvas();
+	document.getElementById('canvas').addEventListener('click', function(e){onClickOnCanvas(e);});
+	writeMatrixInMatirxPreview();
 }
 
 function createCanvas(){
 	canvasDiv.innerHTML = '';
 	let divInfo = canvasDiv.getBoundingClientRect();
-	let divWidth = divInfo.width;
-	let divHeight = divInfo.height; 
-	
-	calculateSquaresSizes(divHeight, divWidth);
-	canvasHeight = settings.squareSize * settings.verticalSquares;
-	canvasWidht = settings.squareSize * settings.horizontalSquares;
+	let div = {
+		height: divInfo.height,
+		width: divInfo.width
+	}; 
+	calculateSquaresSizes(div);
+	let canvasHeight = settings.squareSize * settings.verticalSquares;
+	let canvasWidht = settings.squareSize * settings.horizontalSquares;
 	canvasDiv.innerHTML = '<canvas id=' + canvasId + ' width=' + canvasWidht + ' height=' + canvasHeight + '></canvas>';
-	addMarginToCanvas(divHeight, divWidth);
+	addMarginToCanvas(div);
 }
 
-function calculateSquaresSizes(divHeight, divWidth){
-	if(divWidth / settings.horizontalSquares < divHeight / settings.verticalSquares) {
-		settings.squareSize = divWidth / settings.horizontalSquares;
+function calculateSquaresSizes(div){
+	if(div.width / settings.horizontalSquares < div.height / settings.verticalSquares) {
+		settings.squareSize = div.width / settings.horizontalSquares;
 	} else {
-		settings.squareSize = divHeight / settings.verticalSquares
+		settings.squareSize = div.height / settings.verticalSquares
 	}
 }
 
-function addMarginToCanvas(divHeight, divWidth) {
-	marginVertival = (divHeight - (settings.verticalSquares * settings.squareSize)) / 2;
-	marginHorizontal = (divWidth - (settings.horizontalSquares * settings.squareSize)) / 2;
+function addMarginToCanvas(div) {
+	let marginVertival = (div.height - (settings.verticalSquares * settings.squareSize)) / 2;
+	let marginHorizontal = (div.width - (settings.horizontalSquares * settings.squareSize)) / 2;
 
 	canvasDiv.style.paddingTop =  marginVertival;
 	canvasDiv.style.paddingBottom =  marginVertival;
 	canvasDiv.style.paddingLeft =  marginHorizontal;
 	canvasDiv.style.paddingRight =  marginHorizontal;
-}
-
-function selectColor(e) {
-	let elements = document.getElementsByClassName('selected');
-
-	while(elements.length > 0){
-	    elements[0].classList.remove('selected');
-	}
-	let index =  e.getAttribute("data");
-	settings.selected = index;
-	e.classList.add("selected");
 }
 
 function drawColorMenu() {
@@ -87,15 +112,57 @@ function drawColorMenu() {
 		}
 		content += '" data="' + i + '" onclick="selectColor(this)">';
 		content += '<div><input type="color" value="' + settings.colors[i]; 
-		content += '" onchange="updateMatrixItem(this,';
-		content += "'colors'" + ')" data="' + i + '"></div>';
+		content += '" onchange="updateMatrixColor(this)" data="' + i + '"></div>';
 		content += '<div><input type="text" value="' + settings.values[i]; 
-		content += '" onkeyup="updateMatrixItem(this,';
-		content += "'values'" + ')" data="' + i + '"></div>';
+		content += '" onkeyup="updateMatrixValue(this)" data="' + i + '"></div>';
 		content += '</div>';
 	}
 
 	colorMenu.innerHTML = content;
+}
+
+function updateMatrixColor(e){
+	let value = e.value;
+	let index =  e.getAttribute("data");
+	if (value != "") {
+		settings.colors[index] = value;
+		renderWorkingArea();
+	}	
+}
+
+function updateMatrixValue(e){
+	let value = e.value;
+	let index =  e.getAttribute("data");
+	if (value != "") {
+		settings.values[index] = value;
+		renderWorkingArea();
+	}	
+}
+
+function renderCanvas() {
+
+	for(let i = 0; i < settings.horizontalSquares ; i++){
+		for(let j = 0; j < settings.verticalSquares; j++){
+			index = settings.matrix[i][j];
+			let position = {
+				y: i,
+				x: j
+			}
+			drawSquare(position, settings.colors[index]);
+		}
+	}
+}
+
+function drawSquare(position, color) {
+	const cvs = document.getElementById(canvasId); 
+	const ctx = cvs.getContext("2d");
+	let squareSize = settings.squareSize;
+	
+	ctx.fillStyle = color;
+	ctx.fillRect(position.x*squareSize , position.y*squareSize , squareSize , squareSize );
+
+	ctx.strokeStyle = "#333";
+	ctx.strokeRect(position.x*squareSize , position.y*squareSize , squareSize , squareSize );
 }
 
 function writeMatrixInMatirxPreview() {
@@ -126,101 +193,62 @@ function writeMatrixInMatirxPreview() {
 	matirxPreview.innerHTML = content;
 }
 
+function selectColor(e) {
+	let elements = document.getElementsByClassName('selected');
+
+	while(elements.length > 0){
+	    elements[0].classList.remove('selected');
+	}
+	let index =  e.getAttribute("data");
+	settings.selected = index;
+	e.classList.add("selected");
+}
+
 function onClickOnCanvas(e){
+	let clickedPosition = getClickedSquareOnCanvas(e);
+	addChangeToHistory(clickedPosition);
+	removeStpesHigherThenHistoryIndex();
+	setSelectedValueIntoMatrix(clickedPosition);
+	renderWorkingArea();
+}
+
+function getClickedSquareOnCanvas(e) {
 	let canvas = document.getElementById(canvasId).getBoundingClientRect();
 	let x = e.clientX - canvas.x;
 	let y = e.clientY - canvas.y;
 
 	x = Math.floor(x / settings.squareSize);
 	y = Math.floor(y / settings.squareSize);
-	position = [y, x];
-	addChangeToHistory(position);
-	paintHistory.stepList = paintHistory.stepList.slice(0,(paintHistory.undoIndex +1))
-	settings.matrix[y][x] = parseInt(settings.selected);
-	renderWorkingArea();
+	return {y, x};
 }
 
 function addChangeToHistory (position) {
-	let valueIndex = settings.matrix[position[0]][position[1]];
+	let valueIndex = settings.matrix[position.y][position.x];
 	const data = {
-		position: position,
+		position: [position.y , position.x],
 		oldValue: valueIndex,
 		newValue: settings.selected
 	}
 	paintHistory.undoIndex +=1;
 	paintHistory.stepList.push(data);
-
 }
 
-// Update de Color or the value from a matrix item (the second variable tells if the item is a color or a value)
-function updateMatrixItem(e, colorValue){
-	let value = e.value;
-	let index =  e.getAttribute("data");
-	if (value != "") {
-		if (colorValue == "colors"){
-			settings.colors[index] = value;
-		} else {
-			settings.values[index] = value;
-		}
-		renderWorkingArea();
-	}	
+function removeStpesHigherThenHistoryIndex(){
+	paintHistory.stepList = paintHistory.stepList.slice(0,(paintHistory.undoIndex +1));
+}
+
+function setSelectedValueIntoMatrix(position){
+	settings.matrix[position.y][position.x] = parseInt(settings.selected);
 }
 
 function addNewColorButtonAction() {
 	settings.colors.push("#FFFFFF");
 	settings.values.push("0");
 
-	drawColorMenu(settings.colors, settings.values, settings.selected, colorMenu);
+	drawColorMenu();
 }
 
-function renderWorkingArea() {
-	createCanvas();
-	renderCanvas();
-	document.getElementById('canvas').addEventListener('click', function(e){onClickOnCanvas(e);});
-	writeMatrixInMatirxPreview();
-}
-
-function drawSquare(x, y, color) {
-	const cvs = document.getElementById(canvasId); 
-	const ctx = cvs.getContext("2d");
-	let squareSize = settings.squareSize;
-	
-	ctx.fillStyle = color;
-	ctx.fillRect(x*squareSize , y*squareSize , squareSize , squareSize );
-
-	ctx.strokeStyle = "#333";
-	ctx.strokeRect(x*squareSize , y*squareSize , squareSize , squareSize );
-}
-
-function renderCanvas() {
-
-	for(let i = 0; i < settings.horizontalSquares ; i++){
-		for(let j = 0; j < settings.verticalSquares; j++){
-			index = settings.matrix[i][j];
-			drawSquare(j, i, settings.colors[index]);
-		}
-	}
-}
-
-initialForm.onsubmit = function (e){
-	e.preventDefault();
-	settings.horizontalSquares = document.getElementById('horizontal').value;
-	settings.verticalSquares = document.getElementById('vertical').value;
-	if ( settings.horizontalSquares > 0 && settings.verticalSquares > 0){
-		initialSettings.style.display = 'none';
-		workingArea.style.display = 'flex';
-		settings.matrix = createNewMatrixFilledWithZeros();
-		renderWorkingArea();
-		drawColorMenu(settings.colors, settings.values, settings.selected, colorMenu);
-
-	} else {
-		alert("Please fill in the amount of horizontal and vertical squares!");
-	}	
-};
-
-// Redraw the canvas on the right size after changing the size of the canvas
-window.addEventListener("resize", function(){renderWorkingArea();}, true);
-
+window.addEventListener('wheel', function(e) {zoom(e);}, false);
 function zoom(e){
 	if(e.deltaY > 0 && settings.squareSize > 10){
 		canvasDiv.innerHTML = "";
@@ -240,11 +268,10 @@ function zoom(e){
 	}
 }
 
-window.addEventListener('wheel', function(e) {zoom(e);}, false);
+// Redraw the canvas on the right size after changing the size of the canvas
+window.addEventListener("resize", function(){renderWorkingArea();}, true);
  
-
 document.addEventListener('keydown', (e) => {keyPress(e);});
-
 function keyPress(e){
 	if(e.keyCode === 90 && e.ctrlKey === true && e.shiftKey === false){
 		e.preventDefault();
@@ -257,16 +284,16 @@ function keyPress(e){
 
 function undoStep(e){
 	e.preventDefault();
-	if( paintHistory.undoIndex > 0 ){
+	if( paintHistory.undoIndex >= 0 ){
 		let index = paintHistory.undoIndex;
 		let data = paintHistory.stepList[index];
 		settings.matrix[data.position[0]][data.position[1]] = data.oldValue;
 		paintHistory.undoIndex -= 1;
-		renderCanvas();
-	} else if (paintHistory.undoIndex === 0) {
-		settings.matrix = createNewMatrixFilledWithZeros();
-		paintHistory.undoIndex == -1;
-		renderCanvas();
+		renderWorkingArea();
+	} else {
+		createNewMatrixFilledWithZeros();
+		paintHistory.undoIndex == -2;
+		renderWorkingArea();
 	}
 }
 
@@ -277,6 +304,6 @@ function redoStep(e) {
 		let index = paintHistory.undoIndex;
 		let data = paintHistory.stepList[index];
 		settings.matrix[data.position[0]][data.position[1]] = data.newValue;
-		renderCanvas();
+		renderWorkingArea();
 	}
 }
